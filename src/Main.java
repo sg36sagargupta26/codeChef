ee5f81b95e69d5c36a8fef8da9744253df756b1c
sg36sagargupta26 <sg36.sagargupta26@gmail.com>
2025-01-07 07:54:58 +0530

    Easy Pronunciation : https://www.codechef.com/practice/course/1-star-difficulty-problems/DIFF1200/problems/EZSPEAK

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,50 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.lang.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+class Codechef
+{
+    public static void main (String[] args) {
+        List<String> list = inputList();
+        list.stream()
+                .map(Codechef::isComplexString)
+                .map(a -> a?"NO":"YES")
+                .forEach(System.out::println);
+
+
+    }
+
+    private static List<String> inputList() {
+        List<String> list = new ArrayList<>();
+        Scanner scan = new Scanner(System.in);
+        int totalInput = scan.nextInt();
+        for(int i =0;i<totalInput;i++){
+            int n =scan.nextInt();
+            String s = scan.next();
+            s = s.substring(0,n);
+            list.add(s);
         }
+        return list;
+    }
+
+    private static boolean isComplexString(String string){
+        char [] charArray = string.toCharArray();
+        int count =0 ;
+        for (char c : charArray) {
+            if (isVowel(c)) {
+                count = 0;
+            } else {
+                count++;
+            }
+            if (count >= 4) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean isVowel(char c) {
+        return c=='a' || c=='e' || c=='i' || c=='o' || c=='u';
     }
 }
